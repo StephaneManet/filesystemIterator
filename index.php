d1e8005b536918e6365d625198c361d8e03e5482
Stéphane Manet <reg@stephanemanet.com>
2017-07-11 15:13:08 +0200

    Update index.php

--- index.php
+++ index.php
@@ -7,6 +7,11 @@
   <link rel="stylesheet" href="//code.jquery.com/ui/1.12.1/themes/base/jquery-ui.css">
   <script src="https://code.jquery.com/jquery-1.12.4.js"></script>
   <script src="https://code.jquery.com/ui/1.12.1/jquery-ui.js"></script>
+  
+  <style>
+  /* Un tout petit peu de CSS, qui ne gère véritablement que le footer */
+  #fspFooter { border:1px solid #c5c5c5; margin-top:5px; border-radius:3px; text-align: center; font-size: 9px;}
+  </style>
 </head>
 <body>
  
@@ -18,19 +23,16 @@ $iter = new FilesystemIterator('pages');
 $nbf = 1; // comme "nombre de fichiers", il va permettre d'incrémenter l'id "tab-x" des liens dans la liste
 $nbi = 1; // (comme "nombre d'includes"), il va permettre la même incrémentation, mais pour la partie div
 $fichiers = array();
-
 foreach ($iter as $v) {
-    if ($iter->isFile()) {
-        $fichiers[$v->getMTime()] = $v->getBasename(); // $v->getRealpath();
-    }
-}
+		if ($iter->isFile()) {
+			$fichiers[$v->getMTime()] = $v->getBasename(); // $v->getRealpath();
+		}
+	}
 if ($fichiers) {
-
     krsort($fichiers);
 	/* On affiche les 5 derniers fichiers que l'on classe en tableau */
     $last5 = array_slice($fichiers, 0, 5);
     /* var_dump($last5); */
-
 	/* On commence à afficher la liste des fichiers */
 	echo '<ul>';
     foreach ($last5 as $fichier) {	
@@ -38,16 +40,21 @@ if ($fichiers) {
 		$nbf++;	// on rajoute +1 à la variable pour obtenir #tabs-1, #tabs-2, #tabs-3...	
 	}
 	echo '</ul>';
-
 		
     foreach ($last5 as $fichier) {	
-		echo '<div id="tabs-'.$nbi.'"><p>';
+		echo '<div id="tabs-'.$nbi.'">'; // on peut mettre d'autres balises dans les div, telles que p ou pre.
 		include 'pages/'.$fichier; // on fait un include, il faut donc repréciser le chemin du dossier ici
-		echo '</p></div>';	
+		echo '</div>';	
 		$nbi++;	// idem, on rajoute +1 à la variable pour obtenir #tabs-1, #tabs-2, #tabs-3...	
     }
 }
+else {
+	echo '<p>Merci de rajouter des fichiers dans <a href="/pages">le répertoire "pages"</a>.';
+}
 ?>
+</div>
+<div id="fspFooter">
+<p>Par <a href="http://www.stephanemanet.com">Stéphane Manet</a> sous <a target="_blank" href="LICENSE">licence GNU GPL v3</a></p><a class="github-button" href="https://github.com/stephanemanet" aria-label="Follow @stephanemanet on GitHub">Follow @stephanemanet</a>
 </div>
   <script>
   /* et on attaque le jquery, en l'occurrence on s'appuie sur une fonctionnalité de base de UI */	  
@@ -55,5 +62,7 @@ if ($fichiers) {
     $( "#tabs" ).tabs();
   } );
   </script>
+
+<script async defer src="https://buttons.github.io/buttons.js"></script>  
 </body>
 </html>
