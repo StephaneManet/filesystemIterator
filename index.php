040c62c5e2c281c62357ca27f3bc83d7301e45de
Stéphane Manet <reg@stephanemanet.com>
2017-07-11 12:40:29 +0200

    ajout de quelques commentaires

--- index.php
+++ index.php
@@ -3,9 +3,8 @@
 <head>
   <meta charset="utf-8">
   <meta name="viewport" content="width=device-width, initial-scale=1">
-  <title>Outils</title>
+  <title>File System Parser</title>	
   <link rel="stylesheet" href="//code.jquery.com/ui/1.12.1/themes/base/jquery-ui.css">
-  <link rel="stylesheet" href="/resources/demos/style.css">
   <script src="https://code.jquery.com/jquery-1.12.4.js"></script>
   <script src="https://code.jquery.com/ui/1.12.1/jquery-ui.js"></script>
 </head>
@@ -31,9 +30,7 @@ if ($fichiers) {
 	/* On affiche les 5 derniers fichiers que l'on classe en tableau */
     $last5 = array_slice($fichiers, 0, 5);
     /* var_dump($last5); */
-	
-	
-	
+
 	/* On commence à afficher la liste des fichiers */
 	echo '<ul>';
     foreach ($last5 as $fichier) {	
@@ -53,6 +50,7 @@ if ($fichiers) {
 ?>
 </div>
   <script>
+  /* et on attaque le jquery, en l'occurrence on s'appuie sur une fonctionnalité de base de UI */	  
   $( function() {
     $( "#tabs" ).tabs();
   } );
